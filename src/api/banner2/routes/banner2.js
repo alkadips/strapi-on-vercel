25f1698874ba6657813bcc7ec0e46a311f0ce440
Alka <rashinkaralka@gmail.com>
2024-07-03 17:09:45 +0530

    Initial commit

---
+++ src/api/banner2/routes/banner2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * banner2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::banner2.banner2');
---
+++ src/api/banner2/services/banner2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * banner2 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::banner2.banner2');
